8b4bcac90a62a2c0f621c7c6e6c4dcc9a83ba074
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-06-18 10:52:56 -0300

    Calcula polinônio de Taylor que aproxima sen(x) e printa resultado numa tabela

--- atividade1/exercício1/exercicio1.py
+++ atividade1/exercício1/exercicio1.py
@@ -1,17 +1,29 @@
 import matplotlib
 import numpy
 import math
+import gmpy2 # Usado pois math.factorial dá overflow ao converter int para float
+from tabulate import tabulate
 
 X = math.pi / 4
+N_TEST_CASES = [5, 10, 100]
 
 def main():
-    print(f"n = 5: {taylor_series(X, 5)}\nn = 10: {taylor_series(X, 10)}\nn = 100: {taylor_series(X, 100)}")
+    results = [2 * taylor_series_sen_x(X, test_case) for test_case in N_TEST_CASES]
+    print(tabulate({
+        "n": N_TEST_CASES,
+        "Aproximação": results
+        },
+        headers="keys",
+        floatfmt=".20f"
+        ))
+    # TODO: Calcular erro percentual relativo
 
 
 
-# Calcula o polinômio de Taylor de grau n no ponto x
-def taylor_series(x: float, n: int) -> float:
-    return sum([math.pow(-1, k) * math.pow(x, 2 * k + 1) / math.factorial(2 * k + 1) for k in range(n + 1)])
+
+# Calcula o polinômio de Taylor que aproxima sen(x) de grau n no ponto x
+def taylor_series_sen_x(x: float, n: int) -> float:
+    return sum([math.pow(-1, k) * math.pow(x, 2 * k + 1) / gmpy2.fac(2 * k + 1) for k in range(n + 1)])
 
 
 if __name__ == "__main__":
