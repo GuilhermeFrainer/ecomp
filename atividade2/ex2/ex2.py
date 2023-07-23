5907a568d28c264b6c0dde3d858173ff1bef2040
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-07-23 12:45:29 -0300

    Pequenas alterações. Algoritmo não funciona.

--- atividade2/ex2/ex2.py
+++ atividade2/ex2/ex2.py
@@ -20,11 +20,12 @@ GROUP_LEARNING = 2
 
 def main():
     solve_by_pso(rosenbrock)
+    return
 
 
 # Função de Rosenbrock para duas variáveis
 def rosenbrock(x: np.ndarray):
-    return np.power(1 - x[0], 2) + 100 * np.power(x[1] - np.power(x[0], 2), 2)
+    return math.pow(1 - x[0], 2) + 100 * math.pow(x[1] - math.pow(x[0], 2), 2)
 
 
 # Encontra o mínimo de func pelo método do PSO
@@ -32,27 +33,28 @@ def solve_by_pso(func: Callable):
     # Inicializa enxame
     particles: list[Particle] = []
     for _ in range(SWARM_SIZE):
-        x = random.randrange(LOWER_BOUND[0], UPPER_BOUND[0])
-        y = random.randrange(LOWER_BOUND[1], UPPER_BOUND[1])
-        particles.append(Particle([x, y], 2, 2))
+        x = float(random.randrange(LOWER_BOUND[0], UPPER_BOUND[0]))
+        y = float(random.randrange(LOWER_BOUND[1], UPPER_BOUND[1]))
+        particles.append(Particle([x, y]))
     
     # Encontra g_best em t = 0
-    g_best = particles[0].pos
+    g_best = particles[0].pos.copy()
     for p in particles:
         if func(p.best) < func(g_best):
-            g_best = p.best
+            g_best = p.best.copy()
 
+    # Roda o algoritmo
     for _ in range(MAX_ITERATIONS):
         for p in particles:
-            p.update_velocity(g_best)
-            p.pos = p.pos + p.vel # Move a partícula
+            p.update_velocity(g_best, INDIVIDUAL_LEARNING, GROUP_LEARNING)
+            p.pos += p.vel # Move a partícula
             if func(p.pos) < func(p.best):
-                p.best = p.pos
+                p.best = p.pos.copy()
                 if func(p.best) < func(g_best):
-                    g_best = p.best
+                    g_best = p.best.copy()
 
-    print(f"Partículas: {particles}")
     print(f"x:{g_best}\nf: {func(g_best)}")
+    return
 
 
 if __name__ == "__main__":
--- atividade2/ex2/particle.py
+++ atividade2/ex2/particle.py
@@ -6,23 +6,22 @@ class Particle:
     pos: np.ndarray # Posição
     vel: np.ndarray # Velocidade
     best: np.ndarray # Posição que gera o melhor valor da função já atingido pela partícula
-    individual_learning_rate: float # Taxa de aprendizado individual
-    group_learning_rate: float # Taxa de aprendizado de grupo
 
-    def __init__(self, pos: list, individual: float, group: float):
+
+    def __init__(self, pos: list):
         self.pos = np.array(pos)
+        self.best = self.pos.copy()
         self.vel = np.array([0.0, 0.0])
-        self.best = self.pos
-        self.individual_learning_rate = individual
-        self.group_learning_rate = group
+
 
     def __repr__(self):
-        return f"{self.pos}"
+        return f"{np.round(self.pos, decimals=3)}"
+
     
     # Atualiza velocidade da partícula
-    def update_velocity(self, g_best: np.ndarray):
-        r1 = np.random.uniform(0, 1)
-        r2 = np.random.uniform(0, 1)
-        self.vel += self.individual_learning_rate * r1 * (self.best - self.pos)
-        self.vel += self.group_learning_rate * r2 * (g_best - self.pos)
+    def update_velocity(self, g_best: np.ndarray, individual_learning: float, group_learning: float):
+        r1 = np.random.uniform()
+        r2 = np.random.uniform()
+        self.vel += individual_learning * r1 * (self.best - self.pos)
+        self.vel += group_learning * r2 * (g_best - self.pos)
 
