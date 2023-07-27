6832b47f5d0701236f549bc87771ea73e6c42875
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-07-26 22:24:59 -0300

    Modelo funciona.

--- atividade2/ex1/ex1.py
+++ atividade2/ex1/ex1.py
@@ -3,44 +3,46 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 
+
 ALPHA = 0.1
-B = 5
 MAX_N = 0.8
 MAX_ITERATIONS = 100
 STARTING_N = 0.1
+MAX_ERROR = 10 ** -5
+
 
 def main():
-    x_axis = np.linspace(0, 2, 100)
-    
+    B = np.linspace(0.1, 5)
     # (i)
     f1 = lambda x: min(ALPHA * x, MAX_N)
-    f1_series = pred_function(f1)
 
     # (ii)
     f2 = lambda x: min(ALPHA * math.sqrt(x), MAX_N)
-    f2_series = pred_function(f2)
+
     # (iii)
     f3 = lambda x: min(ALPHA * math.pow(x, 2), MAX_N)
-    f3_series = pred_function(f3)
 
-    iterations = [i for i in range(MAX_ITERATIONS + 1)]
-
-    figure = plt.figure("Exercício 1a")
-
-    axis = figure.add_subplot(111)
-    axis.plot(iterations, f2_series)
-
+    f1_series = [pred(b, f1) for b in B]
+    f2_series = [pred(b, f2) for b in B]
+    f3_series = [pred(b, f3) for b in B]
+    
+    plt.plot(B, f1_series, 'ob')
+    #plt.plot(B, f2_series)
+    #plt.plot(B, f3_series)
     plt.show()
 
 
-# Calcula série temporal do modelo de Pred dada uma função para n_t
-def pred_function(func: Callable) -> list[float]:
-    pred_list = [B / (1 - STARTING_N)]
-    
-    for i in range(MAX_ITERATIONS):
-        pred_list.append(func(pred_list[i]))
-
-    return pred_list
+# Retorna T_inf para um determinado B e uma função para n_t
+def pred(B: float, func: Callable) -> float:
+    curr_error = MAX_ERROR + 1 # Garante que o loop rode
+    T = [STARTING_N]
+    i = 0
+    # Usa erro para verificar se equilíbrio de longo prazo foi atingido
+    while curr_error > MAX_ERROR:
+        T.append(B / (1 - func(T[i])))
+        curr_error = abs((T[i + 1] - T[i]) / T[i])
+        i += 1
+    return T[i]
 
 
 if __name__=="__main__":
