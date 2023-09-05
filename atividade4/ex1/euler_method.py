5be5fb4ae93e4392ababe2441b124535bbcd69a7
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-09-05 11:52:44 -0300

    Testes unitários para método de Euler.

--- atividade4/ex1/euler_method.py
+++ atividade4/ex1/euler_method.py
@@ -21,3 +21,38 @@ def solve(t_space: np.ndarray, initial_condition: list[float], step: float, func
         x.append([next_item, t])
     return x
 
+
+# Testes
+import unittest
+
+
+# Testa utilizando exemplo 22.1 de CHAPRA (2018)
+class TestEuler(unittest.TestCase):
+    def test_iterate(self):
+        initial_condition = [2, 0]
+        func = lambda x: 4 * np.exp(0.8 * x[1]) - 0.5 * x[0]
+        
+        iter_result = iterate(initial_condition, initial_condition[0], 1, func)
+        self.assertAlmostEqual(iter_result, 5)
+
+    def test_solve(self):
+        initial_condition = [2, 0]
+        step = 1
+        t_space = np.linspace(0, 4, 5)
+        func = lambda x: 4 * np.exp(0.8 * x[1]) - 0.5 * x[0]
+
+        solutions = solve(t_space, initial_condition, step, func)
+        expected_solutions = [
+            2.0,
+            5.0,
+            11.40216,
+            25.51321,
+            56.84931
+        ]
+        for (sol, exp) in zip(solutions, expected_solutions):
+            self.assertAlmostEqual(sol[0], exp, places=5)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
