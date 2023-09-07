22559a69a44b612dc177530d7821cece95f669c8
Guilherme Frainer <guilherme.sfrainer@gmail.com>
2023-09-07 16:21:11 -0300

    Fazendo commit só porque a luz pode cair e preciso acessar isso depois.

--- atividade4/ex1/ex1.py
+++ atividade4/ex1/ex1.py
@@ -1,20 +1,54 @@
-import scipy.integrate as scint
 import numpy as np
 import euler_method as euler
 
 
-STEP = 1
-INITIAL_CONDITION = [2, 0]
+# Parâmetros do problema
+T0 = 300
+T1 = 400
+
+# Parâmetros do método
+Z_GUESS = -13.2075
+X_START = 0
+X_STOP = 10
+X_NUM = 10000 # Aumentar para uma comparação mais precisa
+STEP = (X_STOP - X_START) / X_NUM
+TOLERANCE = 1e-3
+
 
 def main():
-    t_space = np.linspace(0, 4, 5)
-    func = lambda x: 4 * np.exp(0.8 * x[1]) - 0.5 * x[0]
-    
-    
-    solutions = euler.solve(t_space, INITIAL_CONDITION, STEP, func)
+    x_space = np.linspace(X_START, X_STOP, X_NUM)
+    dz = lambda args: -0.05 * (200 - args[0])
+    dT = lambda args: args[1]
+    solutions = solve_by_shooting(x_space, STEP, T0, T1, Z_GUESS, dz, dT)
+    print(f"Final T: {solutions[-1][0]}\nFinal z: {solutions[-1][1]}")
+    print(np.isclose(solutions[-1][0], 400, rtol=TOLERANCE))
 
-    for (y, t) in solutions:
-        print(f"t: {t}\ty: {y}")
+
+# Resolve PVC pelo método do tiro
+def solve_by_shooting(
+    x_space: np.ndarray, # Intervalo limitado pelos "valores de contorno"
+    step: float,
+    initial_condition: float,
+    final_condition: float,
+    initial_guess: float,
+    func1, # Equação 1
+    func2 # Equação 2
+):
+    # Variáveis para recalcular chute
+    recalculate_factor = 5 # Fator pelo qual recalibrar chute
+    overshot_last_time = True
+    
+    
+    i = 0
+    var_history = [[initial_condition, initial_guess]]
+    while i < 1:
+        for (i, x) in enumerate(x_space):
+            next_z = euler.iterate(var_history[i], var_history[i][1], step, func1)
+            next_t = euler.iterate(var_history[i], var_history[i][0], step, func2)
+            var_history.append([next_t, next_z])
+        i += 1
+
+    return var_history
 
 
 # Retorna a solução exata apresentada pelo exercício
